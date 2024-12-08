cb03b9f6e4b93985e6c45d51a6daf025c142d72c
Khan / 이창민 <EeeasyCode@gmail.com>
2024-12-08 23:50:08 +0900

    fix: body decorator 추가

--- src/automations/automation.controller.ts
+++ src/automations/automation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AutomationSubscribeService } from './automation-subscribe.service';
 import { AutomationSubscribeRequest } from './subscribe/dtos/automation-subscribe.request';
 
@@ -10,7 +10,7 @@ export class AutomationController {
 
   @Post('automation/subscribe')
   async automationSubscribeLetter(
-    automationSubscribeRequest: AutomationSubscribeRequest,
+    @Body() automationSubscribeRequest: AutomationSubscribeRequest,
   ) {
     const { domain, email, nickname } = automationSubscribeRequest;
     return await this.automationSubscribeService.subscribe(
